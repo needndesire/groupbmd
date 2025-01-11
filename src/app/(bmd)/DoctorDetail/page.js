d1e6f6ac7418bcb9df3095998961ce7ddc1c6412
neeshu <neeshuhaldaur@gmail.com>
2025-01-11 19:20:55 +0530

    k

--- src/app/(bmd)/DoctorDetail/page.js
+++ src/app/(bmd)/DoctorDetail/page.js
@@ -1,16 +1,17 @@
+'use client'
 import React from 'react'
 
 export default function Page() {
   return (
     <>
-    <section>
-      <div className='p-5'>
-       <p>
-         find doctor detail
-        hi,this is Neeshu Kumar 
-        </p>
-      </div>
-    </section>
+      <section>
+        <div className='p-5'>
+          <p>
+            find doctor detail
+            hi,this is Neeshu Kumar
+          </p>
+        </div>
+      </section>
     </>
   )
 }
